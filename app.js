96be2aa8d980c3e3b6b103b10dbda730017a47ab
tsimeonov <tsimeonov12@gmail.com>
2025-05-24 16:12:54 +0200

    create setPosition()

--- app.js
+++ app.js
@@ -28,3 +28,9 @@ function createGameElement(tag, className) {
 	element.className = className;
 	return element;
 }
+
+// Set the position of snake or food
+function setPosition(element, position) {
+	element.style.gridColumn = position.x;
+	element.style.gridRow = position.y;
+}
