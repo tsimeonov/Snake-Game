e7c5f7ecbcf56f26f9bdf0f25d0c75847218e02a
tsimeonov <tsimeonov12@gmail.com>
2025-05-24 16:02:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 
 const board = document.getElementById('game-board');
 
+// Define game variables
+
+let snake = [{ x: 10, y: 10 }];
+
 // Draw game map, snake, food
 function draw() {
 	board.innerHTML = '';
 	drawSnake();
 }
+
+// Draw snake
+
+function drawSnake() {}
