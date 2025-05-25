4c071d4d9dd0d666e3de0f790964920bf70e63b2
tsimeonov <tsimeonov12@gmail.com>
2025-05-25 16:02:37 +0200

    create move()

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const board = document.getElementById('game-board');
 const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
+let direction = 'right';
 
 // Draw game map, snake, food
 function draw() {
@@ -54,4 +55,18 @@ function generateFood() {
 	const x = Math.floor(Math.random() * gridSize + 1);
 	const y = Math.floor(Math.random() * gridSize + 1);
 	return { x, y };
+}
+
+// Moving the snake
+
+function move() {
+	const head = { ...snake[0] };
+	switch (direction) {
+		case 'right':
+			head.x++;
+			break;
+
+		default:
+			break;
+	}
 }
