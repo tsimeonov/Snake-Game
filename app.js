be197923064dad726b888e9a8f43c845565ce107
tsimeonov <tsimeonov12@gmail.com>
2025-07-19 14:22:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const instructionText = document.getElementById('instruction-text');
 const logo = document.getElementById('logo');
 const score = document.getElementById('score');
 
+const highScoreText = document.getElementById('highScore');
+
 // Define game variables
 const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
+let highScore = 0;
 let direction = 'right';
 let gameInterval;
 let gameSpeedDelay = 200;
