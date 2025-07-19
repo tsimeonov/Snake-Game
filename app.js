ed05e72c9f2e026f904200dfcca6ca6aea23ed72
tsimeonov <tsimeonov12@gmail.com>
2025-07-19 14:06:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -157,6 +157,12 @@ function checkCollision() {
 	const head = snake[0];
 
 	if (head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize) {
-		resetGasme();
+		resetGame();
+	}
+
+	for (let i = 1; i < snake.length; i++) {
+		if (head.x === snake[i].x && snake.y === snake[i].y) {
+			resetGame();
+		}
 	}
 }
